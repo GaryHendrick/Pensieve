7e171ddac0a2febd66f722a8a1e89b9c2c6c0b31
Gary Hendrick <gary.hendrick@gmail.com>
2017-06-09 18:36:26 -0400

    testing framework additions

--- pensieve/gui.py
+++ pensieve/gui.py
@@ -1,3 +1,31 @@
+# encoding: utf-8
+"""
+gui module : controlers and views for use in the pensieve gui
+"""
+#-----------------------------------------------------------------------------
+#  Copyright (c) 2017, Gary Hendrick.
+#
+#  This file is part of Pensieve.
+#
+#    Pensieve is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    Pensieve is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  The full license is in the file LICENSE, distributed with this software.
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+#   Imports
+#-----------------------------------------------------------------------------
 import os
 
 import cv2
---
+++ pensieve/testing/tests/test_theModel.py
@@ -0,0 +1,38 @@
+# encoding: utf-8
+"""
+ TheModle tests : tests for the application's model
+"""
+#-----------------------------------------------------------------------------
+#  Copyright (c) 2017, Gary Hendrick.
+#
+#  This file is part of Pensieve.
+#
+#    Pensieve is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    Pensieve is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  The full license is in the file LICENSE, distributed with this software.
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+#   Imports
+#-----------------------------------------------------------------------------
+from unittest import TestCase
+
+from pensieve.model import TheModel
+
+class TestTheModel(TestCase):
+    def test__valid_source(self):
+        self.fail()
+
+    def test__valid_destination(self):
+        self.fail()
